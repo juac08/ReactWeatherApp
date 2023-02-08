8cb39635fad59781f25638e7791767d866344520
juac08 <junaid.ahmad@futureworks.com>
2023-02-08 12:47:03 +0100

    Chakara UI setup

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,16 @@
+import { Button, Box } from "@chakra-ui/react";
 import React from "react";
 
 /**
  * React Functional Component.
  */
 const Dashboard: React.FC<DashboardOwnProps> = () => {
-  return <>Dashboard</>;
+  return (
+    <Box>
+      <h1>Dashboard</h1>
+      <Button>Button</Button>
+    </Box>
+  );
 };
 
 /**
