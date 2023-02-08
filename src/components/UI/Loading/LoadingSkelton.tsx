dd4a1d449ce7b5d74e0d7fff11860f0d0c55b6df
juac08 <junaid.ahmad@futureworks.com>
2023-02-08 17:06:25 +0100

    tooltip for loading state

--- src/components/UI/Loading/LoadingSkelton.tsx
+++ src/components/UI/Loading/LoadingSkelton.tsx
@@ -6,34 +6,37 @@ import {
   SkeletonText,
 } from "@chakra-ui/react";
 import React from "react";
+import BrandTooltip from "../BrandTooltip/BrandTooltip";
 
 /**
  * React Functional Component.
  */
 const LoadingSkelton: React.FC<LoadingSkeltonOwnProps> = ({ loading }) => {
   return (
-    <Box
-      padding="6"
-      boxShadow="lg"
-      minW={"300px"}
-      borderTop="1px solid purple"
-      borderRadius={"md"}
-    >
-      <Skeleton isLoaded={loading} fadeDuration={1}>
-        <CircularProgress
-          isIndeterminate
-          color="purple.400"
-          role="progressbar"
-          size="100px"
-          thickness={"4px"}
-        >
-          <CircularProgressLabel fontSize={"sm"}>
-            Getting Location
-          </CircularProgressLabel>
-        </CircularProgress>
-        <SkeletonText mt="4" noOfLines={4} spacing="4" skeletonHeight="1" />
-      </Skeleton>
-    </Box>
+    <BrandTooltip label="We are looking for your location. Make sure your location is enabled.">
+      <Box
+        padding="6"
+        boxShadow="lg"
+        minW={"300px"}
+        borderTop="1px solid purple"
+        borderRadius={"md"}
+      >
+        <Skeleton isLoaded={loading} fadeDuration={1}>
+          <CircularProgress
+            isIndeterminate
+            color="purple.400"
+            role="progressbar"
+            size="100px"
+            thickness={"4px"}
+          >
+            <CircularProgressLabel fontSize={"sm"}>
+              Getting Location
+            </CircularProgressLabel>
+          </CircularProgress>
+          <SkeletonText mt="4" noOfLines={4} spacing="4" skeletonHeight="1" />
+        </Skeleton>
+      </Box>
+    </BrandTooltip>
   );
 };
 
